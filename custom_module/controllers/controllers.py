70c7b2017fbebb6855b34072facc85114d3ba0cc
Erick Ramírez Romero <a01640079@tec.mx>
2024-03-07 13:55:30 -0600

    Send .csv over http

--- custom_module/controllers/controllers.py
+++ custom_module/controllers/controllers.py
@@ -1,11 +1,29 @@
 from odoo import http
 import json
+from os import getcwd
+import pandas as pd
+import numpy as np
+import re
 
 class MiControlador(http.Controller):
     @http.route('/fn1', type='json', auth='public', website=True)
     def fileProcessing(self, **kw):
         request_body = http.request.httprequest.data
-        print('Cuerpo de la solicitud2:', request_body)
+        #print('Cuerpo de la solicitud2:', request_body)
         json_data = json.loads(request_body.decode('utf-8'))
+        counter = 0
+        lines = json_data['file'].split("\n")
+        with open("/home/erick/odoo-custom-addons/working.csv", "w") as file:
+            for line in lines:
+                nCells = len(re.findall(r'",', line)) + 1
+                if nCells > 3:
+                    data = data[counter:]
+                    file.write(data)
+                    break
+                counter += 1
+        
+            
         return f"Hola {json_data['file']} llamado por {json_data['calledBy']}!"
+    
+
         
--- custom_module/static/src/components/sales_dashboard.js
+++ custom_module/static/src/components/sales_dashboard.js
@@ -23,13 +23,25 @@ export class OwlSalesDashboard extends Component {
 
     async handleFileUpload(file, calledBy) {
         console.log('File:', file);
+        // Create a promise that resolves with the file content
+        const fileContentPromise = new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.onload = (e) => resolve(e.target.result);
+            reader.onerror = (e) => reject(new Error('Failed to read file'));
+            reader.readAsText(file);
+        });
+
         try {
+            // Wait for the file to be read
+            const text = await fileContentPromise;
+            console.log('File Content:', text);
+
             const response = await fetch('/fn1', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({file, calledBy}),
+                body: JSON.stringify({file: text, calledBy}),
             });
             // Handle the response here
             const data = await response.json();
